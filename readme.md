9851e07bbbb3e9b06337b50959d1fb7cc251254c
Daniel Neal <danielneal@riverford.co.uk>
2019-09-26 12:37:52 +0300

    Get it working

--- readme.md
+++ readme.md
@@ -1,4 +1,9 @@
 # Instructions
 
 `shadow-cljs watch script`
-`node sourcemap-cljs <path/to/sourcemap> <line> <column>`
+
+node sourcemap.js line:column,line:column <bundle-source-map> <cljs-source-map>
+
+e.g.
+
+node sourcemap.js 11:920442,11:28340,11:31050,11:61043,11:60979,11:920619,11:28422,11:28348,11:31050,11:61043,11:60979,11:1167998,11:22876,11:1167922,11:86569,11:72186,11:809491,11:73158,11:802595 main.ios.map index.js.map
--- src/sourcemap_cljs/core.cljs
+++ src/sourcemap_cljs/core.cljs
@@ -1,6 +1,7 @@
 (ns sourcemap-cljs.core
   (:require ["source-map" :as sourcemap :refer [SourceMapConsumer]]
-            ["fs" :as fs]))
+            ["fs" :as fs]
+            [clojure.string :as string]))
 
 (enable-console-print!)
 
@@ -10,27 +11,39 @@
 
 (defn main
   [& args]
-  (let [[l c p & [p2]] args
-        l (js/parseInt l)
-        c (js/parseInt c)
-        m (parse-file p)]
-    (.with
-      SourceMapConsumer
-      m
-      nil
-      (fn [consumer]
-        (let [pos (.originalPositionFor consumer #js {:line l
-                                                      :column c})
-              line (.-line pos)
-              column (.-column pos)]
-          (println pos)
-          (when p2
-            (let [m2 (parse-file p2)]
-              (.with
-                SourceMapConsumer
-                m2
-                nil
-                (fn [consumer]
-                  (let [pos (.originalPositionFor consumer #js {:line line
-                                                                :column column})]
-                    (println pos)))))))))))
+  (let [[lcs p1 p2 & [all?]] args
+        m1 (parse-file p1)
+        m2 (parse-file p2)
+        all? (some? all?)]
+    (-> (reduce (fn [p lc]
+                  (-> p
+                      (.then (fn []
+                               (let [[a l c] (re-matches #"(\d+):(\d+)" (string/trim lc))
+                                     l (js/parseInt l)
+                                     c (js/parseInt c)]
+                                 (.with
+                                   SourceMapConsumer
+                                   m1
+                                   nil
+                                   (fn [consumer]
+                                     (let [pos (.originalPositionFor consumer #js {:line l
+                                                                                   :column c})
+                                           line (.-line pos)
+                                           column (.-column pos)]
+                                       (.catch (.with
+                                                 SourceMapConsumer
+                                                 m2
+                                                 nil
+                                                 (fn [consumer]
+                                                   (let [pos2 (.originalPositionFor consumer #js {:line (inc line)
+                                                                                                  :column column})
+                                                         ret (js->clj pos2 :keywordize-keys true)]
+                                                     (when (or all? (.startsWith (:source ret) "riverford"))
+                                                       (println lc)
+                                                       (println ret)))))
+                                               (fn []))))))
+                               ))))
+                (.resolve js/Promise)
+                (string/split lcs #","))
+        (.catch (fn []
+                  (println "An error occurred"))))))

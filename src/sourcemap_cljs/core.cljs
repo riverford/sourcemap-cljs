6b198436a865561dccb1e4b5ad1f5ac9a167084c
Daniel Neal <danielneal@riverford.co.uk>
2019-09-26 10:58:24 +0300

    Follow twice

--- src/sourcemap_cljs/core.cljs
+++ src/sourcemap_cljs/core.cljs
@@ -4,17 +4,33 @@
 
 (enable-console-print!)
 
+(defn parse-file
+  [path]
+  (.parse js/JSON (.readFileSync fs path)))
+
 (defn main
   [& args]
-  (let [[p l c] args
-        m (.parse js/JSON (.readFileSync fs p))
+  (let [[l c p & [p2]] args
         l (js/parseInt l)
-        c (js/parseInt c)]
+        c (js/parseInt c)
+        m (parse-file p)]
     (.with
       SourceMapConsumer
       m
       nil
       (fn [consumer]
         (let [pos (.originalPositionFor consumer #js {:line l
-                                                      :column c})]
-          (println pos))))))
+                                                      :column c})
+              line (.-line pos)
+              column (.-column pos)]
+          (println pos)
+          (when p2
+            (let [m2 (parse-file p2)]
+              (.with
+                SourceMapConsumer
+                m2
+                nil
+                (fn [consumer]
+                  (let [pos (.originalPositionFor consumer #js {:line line
+                                                                :column column})]
+                    (println pos)))))))))))
